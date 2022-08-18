35accd1941a2a13b21cd8a1a8d5c009e17bc926b
AACHOURMOHAMED <aachourmohameddeveloper@gmail.com>
2022-08-18 16:10:10 +0100

    redux with react components

--- src/pages/Categories.js
+++ src/pages/Categories.js
@@ -1,17 +1,30 @@
 import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { BookStatus } from '../redux/categories/categories';
 import Input from '../components/UI/Input';
 
-const Categories = () => (
-  <div>
-    <Input
-      input={{
-        type: 'button',
-        name: '',
-        value: 'Check status',
-        onChange: () => {},
-      }}
-    />
-  </div>
-);
+const Categories = () => {
+  const categories = useSelector((state) => state.categories);
+  const dispatch = useDispatch();
+  const handleCategoryChange = (e) => {
+    dispatch(BookStatus(e.target.value));
+  };
+
+  return (
+    <div className="categories">
+      <Input
+        input={{
+          type: 'button',
+          name: '',
+          value: 'Check status',
+          onClick: handleCategoryChange,
+        }}
+      />
+      <h2>
+        {categories}
+      </h2>
+    </div>
+  );
+};
 
 export default Categories;
